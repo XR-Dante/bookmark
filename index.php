e8e271dce64ea33b4bcc63f83c66bec43f574a13
XR-Dante <telegram77012@gmail.com>
2025-04-17 11:54:35 +0500

    Create docker file and BUG fixed

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-<?php echo "Tasks"; ?>
+try {
 
-</body>
-</html>
+  $conn = new PDO("pgsql:host=postgres;port=5432;dbname=users", "user", "password");
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $stmt = $conn->query("SELECT * FROM public.student");
+  $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach($res as $user){
+    echo  $user['name'] . "<br>";
+  }
+
+} catch(PDOException $e){
+  echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+
